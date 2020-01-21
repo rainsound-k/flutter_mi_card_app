8e2042ef04468ac0bf5c0da0421ce923dea9d635
rainsound-k <rainsound128@gmail.com>
2020-01-21 22:05:16 +0900

    Row, Column 연습

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.white,
+                child: Text('container 1'),
+              ),
+              SizedBox(
+                width: 20.0,
+              ),
+              Container(
+                  width: 100.0,
+                  color: Colors.blue,
+                  child: Text('container 2')),
+              Container(
+                height: 100.0,
+                color: Colors.red,
+                child: Text('container 3'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
